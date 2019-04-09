45342236d27d736da224a38dcfd16e21e78efc21
Arthur McGregor <arthur@orbica.world>
2019-04-09 13:17:52 +1200

    IE friendly default params

--- src/utils/eventListener.js
+++ src/utils/eventListener.js
@@ -1,5 +1,6 @@
 // Shorthand for muliple events with the same function
-export default function (types, func, state = 'add') {
+export default function (types, func) {
+  var state = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : "add";
   types.forEach((type) => {
     document[`${state}EventListener`](type, func);
   });
